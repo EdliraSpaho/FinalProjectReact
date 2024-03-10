e356f952676824922e17af9ac3de438b41f6328f
Eda1234 <spaho_eda@yahoo.com>
2024-03-10 22:45:53 +0100

    Header

--- Final-Project/src/App.tsx
+++ Final-Project/src/App.tsx
@@ -1,24 +1,15 @@
-/*import Header from './Components/Header/Header'
+
+import { Outlet } from "react-router-dom";
+import Header from "./Components/Header/Header";
 
 const App = () => {
   return (
-    <div>
+    <>
       <Header />
-    </div>
-  )
-}
-
-export default App*/
-import { Link, Outlet } from 'react-router-dom';
-
-const App = () => {
-  return (
-    <div>
-      <h1>Home  <Link to={'films'}>The Studio Ghibli Collection</Link></h1>
       <Outlet />
       <footer>Footer</footer>
-    </div>
-  )
+    </>
+  );
 };
 
 export default App;
---
+++ Final-Project/src/Components/Header/Header.tsx
@@ -0,0 +1,21 @@
+import { SearchOutlined } from "@ant-design/icons";
+import { Link } from "react-router-dom";
+import styles from './Styles.module.css';
+
+const Header = () => {
+  return (
+    <div className={styles.headerContainer}>
+      <div className={styles.searchContainer}>
+        <button className={styles.searchButton}>
+          <SearchOutlined /> Search
+        </button>
+      </div>
+      <div className={styles.linksContainer}>
+        <h1><Link to={'films'}>Home</Link></h1>
+        <h1><Link to={''}>My Favorites</Link></h1>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
