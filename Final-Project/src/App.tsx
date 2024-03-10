02a734e65c178ae01c549ea73463ca822052ac66
Eda1234 <spaho_eda@yahoo.com>
2024-03-10 23:20:11 +0100

    Footer

--- Final-Project/src/App.tsx
+++ Final-Project/src/App.tsx
@@ -1,13 +1,14 @@
 
 import { Outlet } from "react-router-dom";
 import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
 
 const App = () => {
   return (
     <>
       <Header />
       <Outlet />
-      <footer>Footer</footer>
+      <Footer />
     </>
   );
 };
---
+++ Final-Project/src/Components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import styles from './Styles.module.css';
+const Footer = () => {
+  return (
+    <div>
+        <div className={styles.footerContainer}>
+        <p>@ 2024</p>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Footer
